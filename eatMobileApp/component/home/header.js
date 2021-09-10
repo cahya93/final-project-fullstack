30b0880184e37b0cea262188058e60e6b2455b1c
cahya93 <ekocahyanto007@gmail.com>
2021-09-10 14:44:43 +0700

    update componen header

--- eatMobileApp/component/home/header.js
+++ eatMobileApp/component/home/header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {Alert} from 'react-native';
+import {Input} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Entypo';
 class header extends Component {
   constructor(props) {
@@ -8,49 +9,20 @@ class header extends Component {
   }
   render() {
     return (
-      <View style={styles.header}>
-        <View style={styles.headerIcon}>
+      <Input
+        placeholder="input your location"
+        leftIcon={<Icon size={25} color={'green'} name="location" />}
+        rightIcon={
           <Icon
-            style={{marginTop: 0}}
-            size={25}
-            color={'green'}
-            name="location"
-          />
-        </View>
-        <View style={styles.headerLocation}>
-          <Text>Your Location</Text>
-        </View>
-        <View style={styles.headerSetting}>
-          <Icon
-            style={{marginTop: 0}}
             size={25}
             color={'green'}
             name="shopping-cart"
+            onPress={() => Alert.alert('keranjang')}
           />
-        </View>
-      </View>
+        }
+      />
     );
   }
 }
-const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-    borderWidth: 1,
-    padding: 5,
-  },
-  headerIcon: {
-    paddingLeft: 5,
-    flex: 0.1,
-  },
-  headerLocation: {
-    flex: 0.8,
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'baseline',
-  },
-  headerSetting: {
-    flex: 0.1,
-    justifyContent: 'flex-end',
-  },
-});
+
 export default header;
