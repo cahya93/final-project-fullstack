f0f5591badd9cdd3a76e8b6fc6c7e12e2537cf8b
cahya93 <ekocahyanto007@gmail.com>
2021-09-10 18:49:40 +0700

    added redux

--- eat-frontend/src/App.js
+++ eat-frontend/src/App.js
@@ -6,13 +6,23 @@
  */
 
 import { Nav, Body, Footer } from "./template";
+import { connect } from "react-redux";
+import ApiBackend from "./service/API/api";
 import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
+  getProduct = () => {
+    ApiBackend.getAllData().then((data) => {
+      console.log(`data`, data);
+      this.props.product(data);
+    });
+  };
+  async componentDidMount() {
+    await this.getProduct();
+  }
   render() {
     return (
       <>
@@ -24,4 +34,7 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  product: (product) => dispatch({ type: "product", payload: { product } }),
+});
+export default connect(null, mapDispatchToProps)(App);
