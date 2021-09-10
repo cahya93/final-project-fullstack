1b794b883ce92bee63d0cfd1e8d682212ba91319
cahya93 <ekocahyanto007@gmail.com>
2021-09-11 02:25:46 +0700

    add api viewbylokasi

--- eat-frontend/src/service/API/api.js
+++ eat-frontend/src/service/API/api.js
@@ -1,5 +1,4 @@
 const API_BASE_URL = "http://yantodev.ddns.net:8080/";
-
 class apiBackend {
   getAllData = () => {
     return fetch(API_BASE_URL + "eats/viewall", {
@@ -14,6 +13,23 @@ class apiBackend {
         console.log("api data", allData);
         return allData;
       });
+  };
+  //get by lokasi
+  getLokasi = () => {
+    return fetch(API_BASE_URL + "eats/viewbylokasi", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      mode: "no-cors",
+      body: JSON.stringify({
+        id_lokasi: 2,
+      }),
+    })
+      .then((response) => response.text())
+      .then((data) => console.log("cek", data))
+      .catch((error) => console.log("Error detected: " + error));
   };
 }
 
